2cf17c3013f7d254e57ea4f108383153ab2a88ce
dmintz7 <dmintz37@gmail.com>
2021-01-25 11:21:20 -0500

    Change Name

--- RemoveEmptyFolders.py
+++ RemoveEmptyFolders.py
@@ -1,8 +1,8 @@
 import os, sys, config, logging
 from logging.handlers import RotatingFileHandler
 
-extra = {'folder_name': os.path.dirname(os.path.abspath(__file__)).split("/")[-1]}
-formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(folder_name)15s:%(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
+filename, file_extension = os.path.splitext(os.path.basename(__file__))
+formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(module)15s:%(funcName)30s:%(lineno)5s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stdout)
@@ -10,10 +10,9 @@ consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 logging.getLogger("requests").setLevel(logging.WARNING)
 logger.setLevel(config.LOG_LEVEL)
-fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/RemoveEmptyFolders.log', maxBytes=1024 * 1024 * 1, backupCount=1)
+fileHandler = RotatingFileHandler(config.LOG_FOLDER + '/' + filename + '.log', maxBytes=1024 * 1024 * 1, backupCount=1)
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
-logger = logging.LoggerAdapter(logger, extra)
 
 review = ""
 total_folders = 0
